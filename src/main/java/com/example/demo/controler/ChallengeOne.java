e2a3d0a5c2d00c4db794964c4fbc73986a92085f
alex <alexandre.moreau.2@ens.etsmtl.ca>
2020-03-05 22:58:28 -0500

    WIP init answer call

--- src/main/java/com/example/demo/controler/ChallengeOne.java
+++ src/main/java/com/example/demo/controler/ChallengeOne.java
@@ -2,33 +2,39 @@ package com.example.demo.controler;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 
 @Controller
 public class ChallengeOne {
-    @GetMapping("/1")
-    public String getChallengeOne() throws IOException {
+    @GetMapping("/ch1/1")
+    public String getChallengeOne() {
         //init();
         return "ch1";
     }
 
+    @GetMapping("/ch1/1/rep")
+    @ResponseBody
+    public String sendRep(@RequestParam String rep) {
+        return "error";
+    }
+
     private void init() throws IOException {
         BufferedImage bImage = ImageIO.read(new File("src/main/resources/static/image/ch1_bkg.jpg"));
-        String ligne = "0100010101110011011101000010000001100011011001010010000001110001011101010110010100100000011101000111010100100000011000010111001100100000011101010110111001100101001000000110001001101001011001010111001001100101";
+        String ligne = "";
         String solution = "";
         for (int i = bImage.getMinX(); i < bImage.getWidth(); i++) {
             for (int j = bImage.getMinY(); j < bImage.getHeight(); j++) {
-                if (j < ligne.length()) {
-                    if (ligne.charAt(j) == '1') {
-                        bImage.setRGB(i, j, 8224125);
-                    }
-                    solution += ligne.charAt(j);
+                if (ligne.charAt(j % ligne.length()) == '1') {
+                    bImage.setRGB(i, j, (int) (3841 + (Math.random() * ((2 + 2) + 1)) - 2));
                 }
+                solution += ligne.charAt(j % ligne.length());
+
             }
         }
         System.out.println(solution);
